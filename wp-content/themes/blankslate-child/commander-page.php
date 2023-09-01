f541a69f43a6345383324e6803c3e30b3d5b489f
Noah Sciortino <noah.sciortino77@gmail.com>
2023-09-01 18:09:26 +0200

    modification nous rencontrer et ajout du formulaire page commander

--- wp-content/themes/blankslate-child/commander-page.php
+++ wp-content/themes/blankslate-child/commander-page.php
@@ -1,5 +1,27 @@
 <?php /* Template Name: commander-page */ ?>
 <?php include "header.php";?>
-Commander
+<main class="page3">
+    <h1 class="h1p3">Commander</h1>
+    <hr class="line"></hr>
+    <h2 class="textp2 h2p3">Votre commande</h2>
+<div class=commandegout>
+<div class="fraise">
+    <p class="fraisetxt">Fraise<p>
+</div>
+<div class="pamplemousse">
+    <p class="framboisetxt">pample<p>
+    <p class="framboisetxt2">mousse<p> 
+</div>
+<div class="framboise">
+    <p class="framboisetxt">fram<p>
+    <p class="framboisetxt2">boise<p>
+</div>
+<div class="citron">
+    <p class="fraisetxt">citron<p>
+</div>
+</div>
+<hr class="line"></hr>
+<?php 
+echo do_shortcode ("[contact-form-7 id='6e42b91' title='formulaire commander']"); ?>
  <?php include "footer.php";?>
 
